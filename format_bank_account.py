2bd2ce4af6787aec2a2039740ba16ad9b628127d
aspuckti22 <43073443+aspuckti22@users.noreply.github.com>
2018-10-17 12:43:27 -0400

    Add files via upload

---
+++ format_bank_account.py
@@ -0,0 +1,22 @@
+'''
+Created on Oct 17, 2018
+
+@author: aspuckti22
+'''
+
+#Accounts
+name1= input("Please enter the first account holder's name: ")
+accountNumber1= int(input("Please enter the first account number: "))
+accountType1= input("Please enter the type of account: ")
+balance1= float(input("Please enter the balance of the account: "))
+
+name2= input("Please enter the second account holder's name: ")
+accountNumber2= int(input("Please enter the second account number: "))
+accountType2= input("Please enter the type of account: ")
+balance2= float(input("Please enter the balance of the account: "))
+
+name3= input("Please enter the third account holder's name: ")
+accountNumber3= int(input("Please enter the third account number: "))
+accountType3= input("Please enter the type of account: ")
+balance3= float(input("Please enter the balance of the account: "))
+
